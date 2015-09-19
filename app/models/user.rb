be5153aaef1acc815cb6ca38dcd7638762acdaf2
Victor Navarro <victor.matias.navarro@gmail.com>
2015-09-19 17:05:59 -0300

    Validating attributes in Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+      before_save {     self.email  =     email.downcase    }
       has_secure_password
+      VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      validates :name, presence: true, length:{maximum: 60}
+      validates :users_name, presence: true, length:{maximum: 40}, uniqueness: true
+      validates :email, presence: true, length:{maximum: 255},
+      format: { with: valid_email },
+      uniqueness:{ case_sensitive: false}
+      validates :password, length:{minimum: 8}
 end

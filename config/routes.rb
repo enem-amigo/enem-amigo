8a9baeb1ee1cfa3232bdb26e44889016ec860a04
Renata Soares <renata.sds09@gmail.com>
2015-09-23 21:59:46 -0300

    Updating views and error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+      get "sessions/new"
+
+      root                                'static_pages#home'
+      get         'signup'    =>    'users#new'
+      get         'login'       =>    'sessions#new'
+      post        'login'      =>    'sessions#create'
+      delete     'logout'    =>    'sessions#destroy'
+
   resources :users
+
 end

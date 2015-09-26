37456fbccaf2449364a85a9365ca5bde3b38a84e
Victor Navarro <victor.matias.navarro@gmail.com>
2015-09-26 14:44:51 -0300

    Converting indentation to spaces in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-      before_save {     self.email  =     email.downcase    }
-      has_secure_password
-      VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-      validates :name, presence: true, length:{maximum: 60}
-      validates :users_name, presence: true, length:{maximum: 40}, uniqueness: true
-      validates :email, presence: true, length:{maximum: 255},
-      format: { with:  VALID_EMAIL },
-      uniqueness:{ case_sensitive: false}
-      validates :password, length:{minimum: 8}, presence: true
+  before_save {     self.email  =     email.downcase    }
+  has_secure_password
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length:{maximum: 60}
+  validates :username, presence: true, length:{maximum: 40}, uniqueness: true
+  validates :email, presence: true, length:{maximum: 255},
+  format: { with:  VALID_EMAIL },
+  uniqueness: { case_sensitive: false }
+  validates :password, length:{minimum: 8}, presence: true
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,75 @@
+class UserTest < ActiveSupport::TestCase
+
+  test 'should be valid' do
+    create_user
+    assert @user.valid?
+  end
+
+  test 'name should not be blank' do
+    create_user
+    @user.name= ""
+    assert_not @user.valid?
+  end
+
+  test 'name should not have more than 60 character' do
+    create_user
+    @user.name= "*"*65
+    assert_not @user.valid?
+  end
+
+  test 'username should not have more than 40 character' do
+    create_user
+    @user.username="*"*45
+    assert_not @user.valid?
+  end
+
+  test 'username should be unique' do
+    create_user
+    @otheruser= User.new(name: "Example Name Of User", username: "examplinho", email: "example2@example2.com", level: "7", password: "supertesttest", password_confirmation: "supertesttest")
+    assert_not @otheruser.valid?
+  end
+
+  test 'email should not be blank' do
+    create_user
+    @user.email=""
+    assert_not @user.valid?
+  end
+
+  test 'email should not have more than 255 character' do
+    create_user
+    @user.email="*"*256
+    assert_not @user.valid?
+  end
+
+  test 'email should have valid format' do
+    create_user
+    @user.email="thisisnotvalid"
+    assert_not @user.valid?
+  end
+
+  test 'email should be unique' do
+    create_user
+    @otheruser = User.new(name: "Example Name Of User", username: "examplinho", email: "example@example.com", level: "7", password: "supertesttest", password_confirmation: "supertesttest")
+    @otheruser.email="example@example.com"
+    assert_not @otheruser.valid?
+  end
+
+  test 'case sensitive should be false' do
+    create_user
+    @otheruser = User.new(name: "Example Name Of User", username: "examplinho", email: "exaMple@example.com", level: "7", password: "supertesttest", password_confirmation: "supertesttest")
+    assert_not @otheruser.valid?
+  end
+
+  test 'password should have at least 8 characters' do
+    create_user
+    @user.password="this"
+    assert_not @user.valid?
+  end
+
+  private
+  def create_user
+    @user = User.new(name: "Example Name", username: "examplinho", email: "example@example.com", level: "0", password: "supertest", password_confirmation: "supertest");
+    @user.save
+  end
+end
+

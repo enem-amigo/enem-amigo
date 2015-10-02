180367bc0053bdfd6375d91f43a9a5a8e1526f49
Mateus Manuel <mateusmanuel21@hotmail.com>
2015-10-02 09:13:58 -0300

    Fixing redirection to previous page and changing display of errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,12 +22,20 @@ module SessionsHelper
   end
 
   def authenticate_admin
-    redirect_to :back unless current_user.role_admin?
+    redirect_to_back(root_path) unless current_user.role_admin?
   end
 
   def verify_user_permission
     user = User.find(params[:id])
-    redirect_to users_path unless user.id == current_user.id or current_user.role_admin?
+    redirect_to_back(users_path) unless user.id == current_user.id or current_user.role_admin?
+  end
+
+  def redirect_to_back(default = root_path)
+    if !request.env["HTTP_REFERER"].blank? and request.env["HTTP_REFERER"] != request.env["REQUEST_URI"]
+      redirect_to :back
+    else
+      redirect_to default
+    end
   end
 
 end

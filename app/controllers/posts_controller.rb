d20c0fc2fd04c90341b1ca20e8b5007b98bd9fcc
Renata Soares <renata.sds09@gmail.com>
2015-10-19 22:11:17 -0200

    Starting implementation for rating posts
    Signed-off-by: Renata Soares <renata.sds09@gmail.com>
    Signed-off-by: Vitor Barbosa <vitornga15@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,21 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		flash[:notice] = "O comentário foi excluído com sucesso"
+		redirect_to @topic
+	end
+
+	def rate
+		post = Post.find(params[:id])
+		unless current_user.rating.include? post.id
+			current_user.rating.push(post.id)
+		end
+		redirect_to questions_path
+	end
+		
 	private
 
 	def post_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   serialize :accepted_questions, Array
+  serialize :rating, Array
 
   before_save { self.email = email.downcase }
   has_secure_password

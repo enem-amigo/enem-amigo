69aa5cddc704c29daae61e693701ea13bbb6ecb3
Victor Navarro <victor.matias.navarro@gmail.com>
2015-10-25 21:46:14 -0200

    Adding validations to models and making changes to views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
-	belongs_to :topic
+	validates :content, presence: true
+
+	belongs_to :post
+	belongs_to :user
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+	validates :content, presence: true
+
+
 	has_many :comments
 	belongs_to :topic
 	belongs_to :user

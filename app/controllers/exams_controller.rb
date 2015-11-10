15cb3fec740eb300f05914e58d9693fbbde4f549
Vítor Barbosa <vitornga15@gmail.com>
2015-11-10 21:26:37 -0200

    Adding tests and fixing methods for exams_controller
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>
    Signed-off-by: Vítor Barbosa <vitornga15@gmail.com>

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -9,110 +9,68 @@ class ExamsController < ApplicationController
     questions = params[:year_exam] ? Question.where(year: params[:year_exam]) : Question.all
 
     if !questions.empty?
-      exam = Exam.new
-
       @math_questions = questions.where(area: "matemática e suas tecnologias")
       @humans_questions = questions.where(area: "ciências humanas e suas tecnologias")
       @language_questions = questions.where(area: "linguagens, códigos e suas tecnologias")
       @nature_questions = questions.where(area: "ciências da natureza e suas tecnologias")
 
-      exam.questions.push((0...@humans_questions.count).to_a.sample 22)
-      exam.right_answers.push(generate_right_answers(exam, 0))
-      puts '1='*80
-      puts Exam.all.count
-      exam.questions.push((0...@math_questions.count).to_a.sample 22)
-      exam.right_answers.push(generate_right_answers(exam, 1))
-      puts '2='*80
-      puts Exam.all.count
-      exam.questions.push((0...@language_questions.count).to_a.sample 23)
-      puts '3='*80
-      puts Exam.all.count
-      exam.right_answers.push(generate_right_answers(exam, 2))
-      puts '4='*80
-      puts Exam.all.count
+      auxiliar_exam = Exam.new
+      auxiliar_exam.questions.push((0...@humans_questions.count).to_a.sample 22)
+      auxiliar_exam.questions.push((0...@math_questions.count).to_a.sample 22)
+      auxiliar_exam.questions.push((0...@language_questions.count).to_a.sample 23)
+      auxiliar_exam.questions.push((0...@nature_questions.count).to_a.sample 23)
 
-      exam.questions.push((0...@nature_questions.count).to_a.sample 23)
-      exam.right_answers.push(generate_right_answers(exam, 3))
+      @exam = Exam.new
 
-      puts '5='*80
-      puts Exam.all.count
-
-      exam.save
-
-      puts 'final='*80
-      puts Exam.all
-      puts Exam.all.count
-
-      @exams = []
-      @exams.push(exam.id)
       for i in 0...4
-        exam.questions[i].each do |a|
+        auxiliar_exam.questions[i].each do |a|
           if i == 0
-            single_question = Question.where(area: "ciências humanas e suas tecnologias")[a]
+            single_question = @humans_questions[a]
           elsif i == 1
-            single_question = Question.where(area: "matemática e suas tecnologias")[a]
+            single_question = @math_questions[a]
           elsif i == 2
-            single_question = Question.where(area: "linguagens, códigos e suas tecnologias")[a]
+            single_question = @language_questions[a]
           elsif i == 3
-            single_question = Question.where(area: "ciências da natureza e suas tecnologias")[a]
+            single_question = @nature_questions[a]
           end
-          @exams.push(single_question)
+
+          @exam.questions.push(single_question.id)
+          @exam.right_answers.push(single_question.right_answer)
         end
       end
 
-      @exams
+      @exam.save
+      @exam
     else
-      redirect_to_back(exam_result_path)
-      flash[:danger] = "Não há questões cadastradas do ano de #{params[:year_exam]}."
+      redirect_to_back(select_exam_path)
+      if params[:year_exam]
+        flash[:danger] = "Não há questões cadastradas do ano de #{params[:year_exam]}."
+      else
+        flash[:danger] = "Não há questões cadastradas."
+      end
     end
-
   end
 
   def exam_result
-    @exam_user_answers = []
-    @accepted_answers = 0
+    if params[:exam_id]
+      @exam_user_answers = []
+      @accepted_answers = 0
 
-    @current_exam = Exam.find(params[:exam_id])
+      @current_exam = Exam.find(params[:exam_id])
 
-    for i in 0...4
-      @current_exam.questions[i].each do |t|
-        if i == 0
-          question = Question.where(area: "ciências humanas e suas tecnologias")[t]
-        elsif i == 1
-          question = Question.where(area: "matemática e suas tecnologias")[t]
-        elsif i == 2
-          question = Question.where(area: "linguagens, códigos e suas tecnologias")[t]
-        elsif i == 3
-          question = Question.where(area: "ciências da natureza e suas tecnologias")[t]
-        end
-
+      @current_exam.questions.each do |t|
+        question = Question.find(t)
         user_answer = params[:"alternative_#{question.id}"] ? params[:"alternative_#{question.id}"] : "não marcou"
         @exam_user_answers.push(user_answer)
         if(user_answer == question.right_answer)
           @accepted_answers += 1
         end
       end
+      @exam_user_aswers
+    else
+      redirect_to_back
+      flash[:danger] = "Você deve responder uma prova antes para obter seu resultado."
     end
-
-    @exam_user_aswers
   end
-
-  private
-
-    def generate_right_answers exam, index
-      right_answers = []
-        exam.questions[index].each do |t|
-          if index == 0
-            right_answers.push(@humans_questions[t].right_answer)
-          elsif index == 1
-            right_answers.push(@math_questions[t].right_answer)
-          elsif index == 2
-            right_answers.push(@language_questions[t].right_answer)
-          elsif index == 3
-            right_answers.push(@nature_questions[t].right_answer)
-          end
-        end
-      right_answers
-    end
 
 end
--- test/controllers/exams_controller_test.rb
+++ test/controllers/exams_controller_test.rb
@@ -6,6 +6,10 @@ class ExamsControllerTest < ActionController::TestCase
   def setup
     @request.env['HTTP_REFERER'] = 'http://test.host/#'
     @user = users(:renata)
+    @math_question = create_question(2011,"matemática e suas tecnologias")
+    @human_question = create_question(2014,"ciências humanas e suas tecnologias")
+    @language_question = create_question(2011,"linguagens, códigos e suas tecnologias")
+    @nature_question = create_question(2014,"ciências da natureza e suas tecnologias")
   end
 
   test "should get select_exam page when user is logged in" do
@@ -18,5 +22,80 @@ class ExamsControllerTest < ActionController::TestCase
     get :select_exam
     assert_redirected_to login_path
   end
+
+  test "should not get answer_exam page if user is not logged in" do
+    get :answer_exam
+    assert_redirected_to login_path
+  end
+
+  test "should get answer_exam page if there are questions in the database" do
+    log_in @user
+    get :answer_exam
+    assert_response :success
+  end
+
+  test "should not get answer_exam page if there are no questions in the database" do
+    log_in @user
+    Question.destroy_all
+    get :answer_exam
+    assert_redirected_to :back
+  end
+
+  test "should exam have the same number of questions and right_answers when an exam is created" do
+    log_in @user
+    get :answer_exam
+    last_exam = Exam.last
+    assert_equal last_exam.questions.count, last_exam.right_answers.count
+  end
+
+  test "should create exam when user clicks goes to answer_exam page" do
+    log_in @user
+    old_exam = Exam.last
+    get :answer_exam
+    assert_not_equal old_exam, Exam.last
+  end
+
+  test "should create year exam if there are question for that year" do
+    log_in @user
+    old_exam = Exam.last
+    get :answer_exam, year_exam: 2011
+    new_exam = Exam.last
+    assert_not_equal old_exam, new_exam
+  end
+
+  test "should not create year exam if there are no question for that year" do
+    log_in @user
+    old_exam = Exam.last
+    get :answer_exam, year_exam: 2019
+    new_exam = Exam.last
+    assert_equal old_exam, new_exam
+    assert_redirected_to :back
+  end
+
+  test "should right_answers not be empty when an exam is created" do
+    log_in @user
+    get :answer_exam
+    last_exam = Exam.last
+    assert_not last_exam.right_answers.empty?
+  end
+
+  test "should questions not be empty when an exam is created" do
+    log_in @user
+    get :answer_exam
+    last_exam = Exam.last
+    assert_not last_exam.questions.empty?
+  end
+
+  private
+    def create_question year, area
+      @question = Question.new(area: area, enunciation: 'something', number: 001, year: year, right_answer: 'a', image: "", reference: "", text: "")
+      5.times{@question.alternatives.build}
+      @question.alternatives.each do |a|
+        a.letter = 'a'
+        a.description = 'something'
+      end
+      @question.save
+      @question
+    end
 
 end

43e739c472b5a1f6580f2726e8b0015ad61aacdc
Vítor Barbosa <vitornga15@gmail.com>
2015-11-10 10:57:47 -0200

    Starting tests and fixing controller/views for Exam
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>
    Signed-off-by: Vítor Barbosa <vitornga15@gmail.com>

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,6 +1,6 @@
 class ExamsController < ApplicationController
 
-  include ExamsHelper
+  before_action :authenticate_user
 
   def select_exam
   end
@@ -8,47 +8,64 @@ class ExamsController < ApplicationController
   def answer_exam
     questions = params[:year_exam] ? Question.where(year: params[:year_exam]) : Question.all
 
-    exam = Exam.new
-    exam.right_answers = []
+    if !questions.empty?
+      exam = Exam.new
 
-    @math_questions = questions.where(area: "matemática e suas tecnologias")
-    @humans_questions = questions.where(area: "ciências humanas e suas tecnologias")
-    @language_questions = questions.where(area: "linguagens, códigos e suas tecnologias")
-    @nature_questions = questions.where(area: "ciências da natureza e suas tecnologias")
+      @math_questions = questions.where(area: "matemática e suas tecnologias")
+      @humans_questions = questions.where(area: "ciências humanas e suas tecnologias")
+      @language_questions = questions.where(area: "linguagens, códigos e suas tecnologias")
+      @nature_questions = questions.where(area: "ciências da natureza e suas tecnologias")
 
-    exam.questions.push((0...@humans_questions.count).to_a.sample 22)
-    exam.right_answers.push(generate_right_answers(exam, 0))
-    exam.questions.push((0...@math_questions.count).to_a.sample 22)
-    exam.right_answers.push(generate_right_answers(exam, 1))
-    exam.questions.push((0...@language_questions.count).to_a.sample 23)
-    exam.right_answers.push(generate_right_answers(exam, 2))
-    exam.questions.push((0...@nature_questions.count).to_a.sample 23)
-    exam.right_answers.push(generate_right_answers(exam, 3))
-    exam.save
+      exam.questions.push((0...@humans_questions.count).to_a.sample 22)
+      exam.right_answers.push(generate_right_answers(exam, 0))
+      puts '1='*80
+      puts Exam.all.count
+      exam.questions.push((0...@math_questions.count).to_a.sample 22)
+      exam.right_answers.push(generate_right_answers(exam, 1))
+      puts '2='*80
+      puts Exam.all.count
+      exam.questions.push((0...@language_questions.count).to_a.sample 23)
+      puts '3='*80
+      puts Exam.all.count
+      exam.right_answers.push(generate_right_answers(exam, 2))
+      puts '4='*80
+      puts Exam.all.count
 
-    @exams = []
-    @exams.push(exam.id)
-    for i in 0...4
-      exam.questions[i].each do |a|
-        if i == 0
-          single_question = Question.where(area: "ciências humanas e suas tecnologias")[a]
-        elsif i == 1
-          single_question = Question.where(area: "matemática e suas tecnologias")[a]
-        elsif i == 2
-          single_question = Question.where(area: "linguagens, códigos e suas tecnologias")[a]
-        elsif i == 3
-          single_question = Question.where(area: "ciências da natureza e suas tecnologias")[a]
+      exam.questions.push((0...@nature_questions.count).to_a.sample 23)
+      exam.right_answers.push(generate_right_answers(exam, 3))
+
+      puts '5='*80
+      puts Exam.all.count
+
+      exam.save
+
+      puts 'final='*80
+      puts Exam.all
+      puts Exam.all.count
+
+      @exams = []
+      @exams.push(exam.id)
+      for i in 0...4
+        exam.questions[i].each do |a|
+          if i == 0
+            single_question = Question.where(area: "ciências humanas e suas tecnologias")[a]
+          elsif i == 1
+            single_question = Question.where(area: "matemática e suas tecnologias")[a]
+          elsif i == 2
+            single_question = Question.where(area: "linguagens, códigos e suas tecnologias")[a]
+          elsif i == 3
+            single_question = Question.where(area: "ciências da natureza e suas tecnologias")[a]
+          end
+          @exams.push(single_question)
         end
-        @exams.push(single_question)
       end
-    end
 
-    if @exams.count == 1
+      @exams
+    else
       redirect_to_back(exam_result_path)
       flash[:danger] = "Não há questões cadastradas do ano de #{params[:year_exam]}."
-    else
-      @exams
     end
+
   end
 
   def exam_result
@@ -79,5 +96,23 @@ class ExamsController < ApplicationController
 
     @exam_user_aswers
   end
+
+  private
+
+    def generate_right_answers exam, index
+      right_answers = []
+        exam.questions[index].each do |t|
+          if index == 0
+            right_answers.push(@humans_questions[t].right_answer)
+          elsif index == 1
+            right_answers.push(@math_questions[t].right_answer)
+          elsif index == 2
+            right_answers.push(@language_questions[t].right_answer)
+          elsif index == 3
+            right_answers.push(@nature_questions[t].right_answer)
+          end
+        end
+      right_answers
+    end
 
 end
